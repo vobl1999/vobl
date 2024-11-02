7d0521d90b17a0cda6d31875dd75b1482e6d9c5c
vobl1999 <maimaidx@hotmail.com>
2024-11-02 13:36:48 +0800

    上传欢迎界面

---
+++ codes/welcome.py
@@ -0,0 +1,16 @@
+import math
+print('vobl1999 time maimai')
+time.sleep(1)
+#开机#
+print('loading…')
+time.sleep(3)
+#加载休眠3秒#
+print('欢迎')
+#欢迎页面#
+time.sleep(1)
+print('输入1开始对人程序')
+a = input(int())
+if a =  1:
+    open(others.py)
+#############################
+print('')
